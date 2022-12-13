84d15990c96f04701f51665538cade3ff77c9e06
Gary Smith <garyesmith@gmail.com>
2022-12-13 17:02:02 -0500

    Added root path to API.

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const fs = require("fs");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send({"version":"1.0","status":"ok"});
+}
+
 app.get("/posts", (req, res) => {
   fs.readFile('./data/data.json', 'utf8', (err, data) => {
     if (err) {
