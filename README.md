92150b8a6293b2290b8b3b5b671738b33a09a5da
Gary Smith <garyesmith@gmail.com>
2022-12-13 16:58:39 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Placeholder Express API
+
+This repository implements a sample static blog content API, configured for deployment on Vercel.
+
+Thank you to this [simple guide](https://shadowsmith.com/how-to-deploy-an-express-api-to-vercel) describing how to quickly deploy an Express API on Vercel for saving me some time.
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.get("/posts", (req, res) => {
+  fs.readFile('./data/data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  });
+});
+
+app.listen(3000, () => {
+  console.log("API is running on port 3000.");
+});
+
+
+module.exports = app;
